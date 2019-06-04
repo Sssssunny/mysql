9e8e2456dfdfdfbd2641ca7cf07c7c9f44902b62
Sssssunny <hhe330@naver.com>
2019-06-04 11:30:17 +0900

    커밋

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const mysql = require('mysql');
+const pool  = mysql.createPool({
+  connectionLimit : 10,
+  host            : 'localhost',
+  user            : 'root',
+  password        : 'qazx1235',
+  database        : 'sunny'
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  pool.getConnection(function(err, conn){
+    conn.query('SELECT * FROM emp;', function(err, results){
+      console.log('-------------results-------------');
+      console.log(results);
+      console.log('--------------------------');
+
+      res.render('index', { results: results });
+
+      conn.release();
+    });
+  });
 });
 
 module.exports = router;
