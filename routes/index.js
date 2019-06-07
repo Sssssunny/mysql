41879443105c38142148faad79f3fe82166c4e6c
Sssssunny <hhe330@naver.com>
2019-06-07 15:02:55 +0900

    수정

--- routes/index.js
+++ routes/index.js
@@ -21,15 +21,6 @@ router.get('/', function(req, res, next) {
   const id = req.body.id;
   const pw = req.body.pw;
   res.render('login');
-//
-//   console.log(id);
-//   console.log(pw);
-  // conn.query('SELECT * FROM user WHERE `EMAIL` = id;', function(err, results){
-
-  //   res.render('index', { results: results });
-
-  //   conn.release();
-  // });
 });
 
 // 로그인 페이지
@@ -43,9 +34,7 @@ router.post('/', function(req, res, next) {
       if(result.length > 0) {
         res.render('true', {ID: id, PW: pw});
         console.log('---------로그인 성공---------');
-      }
-    });
-    conn.query(`SELECT * FROM user WHERE EMAIL = '${id}' AND PW = password('${pw}');`,function(err, result){
+      } else
       if(result.length >= 0) {
         res.render('false');
         console.log('---------로그인 실패---------');
@@ -87,30 +76,3 @@ router.get('/join', function(req, res, next) {
 });
 
 module.exports = router;
-
-
-
-// app.post('/', function(req, res) {
-//   const id = req.body.id;
-//   const pw = req.body.pw;
-//   const sql = 'SELECT * FROM user';
-//   conn.query(sql, [id], function(err, results){
-//     if(err)
-//       console.log(err);
-
-//     if(!results[0])
-//       return res.send('존재하지 않는 아이디입니다.');
-
-//     const user = results[0];
-//     crypto.pbkdf2(pw, user.salt, 100000, 64, 'sha512', function(err, derivedKey){
-//       if(err)
-//         console.log(err);
-//       if(derivedKey.toString('hex') === user.password){
-//         return res.send('login success');
-//       }
-//       else {
-//         return res.send('존재하지 않는 비밀번호입니다.');
-//       }
-//     });
-//   });
-// });
